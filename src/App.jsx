4557134b3365b28c9f0d256038021319eca0e18a
kshitij-nehete <kshitijnehete@gmail.com>
2025-07-15 21:24:45 +0530

    Initial Setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import Hero from "./components/Hero";
+
+export default function App() {
+  return (
+    <main className="bg-white dark:bg-gray-900 text-gray-900 dark:text-white">
+      <Hero />
+    </main>
+  );
+}
